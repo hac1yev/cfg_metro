97e268d5c6e8b05ea4558186b096dcbf0e9b1f4f
hac1yev <ilkinhaciyev955@gmail.com>
2023-10-12 11:34:11 +0400

    metro slider menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './Header.css';
 import { Link, useLocation } from 'react-router-dom';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Header = () => {
     const [isHomepage,setIsHomepage] = useState(true);
+    const [isMenuOpen,setIsMenuOpen] = useState(false);
 
     const location = useLocation();
     const { pathname } = location;
@@ -28,14 +31,24 @@ const Header = () => {
         }
     }, [pathname]);
 
+    const handleOpenMenu = () => {
+        setIsMenuOpen(true);
+    };
+
+    const handleCloseMenu = () => {
+        setIsMenuOpen(false);
+    };
+    
 
     return (
         <div className={isHomepage ? 'home-header-wrapper' : 'header-wrapper'}>
             <div className='container header-container'>
                 <Link to="/" className='metro-logo'>
                     <img src="https://www.hyhmetro.com.tr/img/logo.png" alt="logo" />
-                    
                 </Link>
+                <div className='hamburger-menu' onClick={handleOpenMenu}>
+                    <RxHamburgerMenu />
+                </div>
                 <ul className='header-menu'>
                     <li className="menu-item">
                         <Link to="/">
@@ -68,6 +81,48 @@ const Header = () => {
                         </Link>
                     </li>
                 </ul>
+                <div className={!isMenuOpen ? 'mobile-menu' : 'mobile-menu open-mobile-menu'}>
+                    <div className='mobile-menu-header'>
+                        <Link to="/" className='metro-logo'>
+                            <img src="https://www.hyhmetro.com.tr/img/logo.png" alt="logo" />
+                        </Link>
+                        <div className='close-menu' onClick={handleCloseMenu}>
+                            <AiOutlineClose />
+                        </div>
+                    </div>
+                    <ul>
+                        <li className="menu-item">
+                            <Link to="/">
+                                Ana sayfa
+                            </Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/guzergah">
+                                Güzergah
+                            </Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/">
+                                Proje Bilgileri
+                            </Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/">
+                                Haberler
+                            </Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/">
+                                Teknik Dokümanlar
+                            </Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/">
+                                İletişim
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
--- src/components/Home/MainSlider.jsx
+++ src/components/Home/MainSlider.jsx
@@ -40,20 +40,26 @@ const MainSlider = () => {
                         <div className='slider-img'>
                             <img src={slider_img1} alt="slider-main-image" />
                         </div>
-                        <div className='slider-content col-md-6'>
-                            <h3>CFG ISTANBUL</h3>
-                            <h1>METRO</h1>
-                            <p>
-                                Credibly leverage existing business experiences through
-                                magnetic mindshare. Synergistically exploit
-                                efficient partnerships world-class applications.
-                            </p>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
-                                    <path d="M11.5 22.61C17.7132 22.61 22.75 17.5732 22.75 11.36C22.75 5.14684 17.7132 0.110046 11.5 0.110046C5.2868 0.110046 0.25 5.14684 0.25 11.36C0.25 17.5732 5.2868 22.61 11.5 22.61Z" fill="#171E2F"/>
-                                    <path d="M11.5 15.9601C14.0405 15.9601 16.1 13.9006 16.1 11.3601C16.1 8.81956 14.0405 6.76007 11.5 6.76007C8.95951 6.76007 6.90002 8.81956 6.90002 11.3601C6.90002 13.9006 8.95951 15.9601 11.5 15.9601Z" stroke="white" stroke-width="0.2"/>
-                                </svg>
-                                <Link to="/">Take A Look</Link>
+                        <div className='w-100 d-flex justify-content-center'>
+                            <div className="container slider-content">
+                                <div className="row">
+                                    <div className='col-lg-6 slider-content-col'>
+                                        <h3>CFG ISTANBUL</h3>
+                                        <h1>METRO</h1>
+                                        <p>
+                                            Credibly leverage existing business experiences through
+                                            magnetic mindshare. Synergistically exploit
+                                            efficient partnerships world-class applications.
+                                        </p>
+                                        <div>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
+                                                <path d="M11.5 22.61C17.7132 22.61 22.75 17.5732 22.75 11.36C22.75 5.14684 17.7132 0.110046 11.5 0.110046C5.2868 0.110046 0.25 5.14684 0.25 11.36C0.25 17.5732 5.2868 22.61 11.5 22.61Z" fill="#171E2F"/>
+                                                <path d="M11.5 15.9601C14.0405 15.9601 16.1 13.9006 16.1 11.3601C16.1 8.81956 14.0405 6.76007 11.5 6.76007C8.95951 6.76007 6.90002 8.81956 6.90002 11.3601C6.90002 13.9006 8.95951 15.9601 11.5 15.9601Z" stroke="white" stroke-width="0.2"/>
+                                            </svg>
+                                            <Link to="/">Take A Look</Link>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -61,20 +67,26 @@ const MainSlider = () => {
                         <div className='slider-img'>
                             <img src={slider_img2} alt="slider-main-image" />
                         </div>
-                        <div className='slider-content col-md-6'>
-                            <h3>CFG ISTANBUL</h3>
-                            <h1>METRO</h1>
-                            <p>
-                                Credibly leverage existing business experiences through
-                                magnetic mindshare. Synergistically exploit
-                                efficient partnerships world-class applications.
-                            </p>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
-                                    <path d="M11.5 22.61C17.7132 22.61 22.75 17.5732 22.75 11.36C22.75 5.14684 17.7132 0.110046 11.5 0.110046C5.2868 0.110046 0.25 5.14684 0.25 11.36C0.25 17.5732 5.2868 22.61 11.5 22.61Z" fill="#171E2F"/>
-                                    <path d="M11.5 15.9601C14.0405 15.9601 16.1 13.9006 16.1 11.3601C16.1 8.81956 14.0405 6.76007 11.5 6.76007C8.95951 6.76007 6.90002 8.81956 6.90002 11.3601C6.90002 13.9006 8.95951 15.9601 11.5 15.9601Z" stroke="white" stroke-width="0.2"/>
-                                </svg>
-                                <Link to="/">Take A Look</Link>
+                        <div className='w-100 d-flex justify-content-center'>
+                            <div className="container slider-content">
+                                <div className="row">
+                                    <div className='col-lg-6 slider-content-col'>
+                                        <h3>CFG ISTANBUL</h3>
+                                        <h1>METRO</h1>
+                                        <p>
+                                            Credibly leverage existing business experiences through
+                                            magnetic mindshare. Synergistically exploit
+                                            efficient partnerships world-class applications.
+                                        </p>
+                                        <div>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
+                                                <path d="M11.5 22.61C17.7132 22.61 22.75 17.5732 22.75 11.36C22.75 5.14684 17.7132 0.110046 11.5 0.110046C5.2868 0.110046 0.25 5.14684 0.25 11.36C0.25 17.5732 5.2868 22.61 11.5 22.61Z" fill="#171E2F"/>
+                                                <path d="M11.5 15.9601C14.0405 15.9601 16.1 13.9006 16.1 11.3601C16.1 8.81956 14.0405 6.76007 11.5 6.76007C8.95951 6.76007 6.90002 8.81956 6.90002 11.3601C6.90002 13.9006 8.95951 15.9601 11.5 15.9601Z" stroke="white" stroke-width="0.2"/>
+                                            </svg>
+                                            <Link to="/">Take A Look</Link>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -82,20 +94,26 @@ const MainSlider = () => {
                         <div className='slider-img'>
                             <img src={slider_img3} alt="slider-main-image" />
                         </div>
-                        <div className='slider-content col-md-6'>
-                            <h3>CFG ISTANBUL</h3>
-                            <h1>METRO</h1>
-                            <p>
-                                Credibly leverage existing business experiences through
-                                magnetic mindshare. Synergistically exploit
-                                efficient partnerships world-class applications.
-                            </p>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
-                                    <path d="M11.5 22.61C17.7132 22.61 22.75 17.5732 22.75 11.36C22.75 5.14684 17.7132 0.110046 11.5 0.110046C5.2868 0.110046 0.25 5.14684 0.25 11.36C0.25 17.5732 5.2868 22.61 11.5 22.61Z" fill="#171E2F"/>
-                                    <path d="M11.5 15.9601C14.0405 15.9601 16.1 13.9006 16.1 11.3601C16.1 8.81956 14.0405 6.76007 11.5 6.76007C8.95951 6.76007 6.90002 8.81956 6.90002 11.3601C6.90002 13.9006 8.95951 15.9601 11.5 15.9601Z" stroke="white" stroke-width="0.2"/>
-                                </svg>
-                                <Link to="/">Take A Look</Link>
+                        <div className='w-100 d-flex justify-content-center'>
+                            <div className="container slider-content">
+                                <div className="row">
+                                    <div className='col-lg-6 slider-content-col'>
+                                        <h3>CFG ISTANBUL</h3>
+                                        <h1>METRO</h1>
+                                        <p>
+                                            Credibly leverage existing business experiences through
+                                            magnetic mindshare. Synergistically exploit
+                                            efficient partnerships world-class applications.
+                                        </p>
+                                        <div>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
+                                                <path d="M11.5 22.61C17.7132 22.61 22.75 17.5732 22.75 11.36C22.75 5.14684 17.7132 0.110046 11.5 0.110046C5.2868 0.110046 0.25 5.14684 0.25 11.36C0.25 17.5732 5.2868 22.61 11.5 22.61Z" fill="#171E2F"/>
+                                                <path d="M11.5 15.9601C14.0405 15.9601 16.1 13.9006 16.1 11.3601C16.1 8.81956 14.0405 6.76007 11.5 6.76007C8.95951 6.76007 6.90002 8.81956 6.90002 11.3601C6.90002 13.9006 8.95951 15.9601 11.5 15.9601Z" stroke="white" stroke-width="0.2"/>
+                                            </svg>
+                                            <Link to="/">Take A Look</Link>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
